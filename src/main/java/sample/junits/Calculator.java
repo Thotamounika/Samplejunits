5711d435c318dd9b19c11df88264853a529d1582
ThotaMounika <thotasrinagamounika@gmail.com>
2019-02-10 23:50:53 +0530

    local initial commit

---
+++ src/main/java/sample/junits/Calculator.java
@@ -0,0 +1,12 @@
+package sample.junits;
+
+public class Calculator {
+
+	
+	public int add(int i,int j){
+		return i+j;
+	}
+	
+	
+	
+}
---
+++ src/test/java/sample/junits/CalculatorTest.java
@@ -0,0 +1,17 @@
+package sample.junits;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	
+	
+	Calculator c=new Calculator();
+	@Test
+	public void testAdd() {
+		assertEquals(5,c.add(2, 3));
+	}
+
+}
